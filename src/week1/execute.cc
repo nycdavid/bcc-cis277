0c902137ab8e7c7e7e6890d496b8c2d2c8762dcd
David Ko <david.ko@pvtmethod.com>
2017-09-04 14:36:28 -0400

    Coding portion is complete

--- src/week1/execute.cc
+++ src/week1/execute.cc
@@ -3,18 +3,68 @@
 #include "./vector.hpp"
 
 int main() {
-  std::ofstream output_file;
-  output_file.open("output.txt");
+  // 3. Vector Addition
+  // ------------------
+  // Vector vct1 = get_vector();
+  // Vector vct2 = get_vector();
+  // Vector sum = { "Sum", 0, 0 };
+  // calc_sum(vct1, vct2, sum);
+  //
+  // std::ofstream output_file;
+  // output_file.open("output.txt");
+  // print_vector(output_file, vct1);
+  // print_vector(output_file, vct2);
+  // print_vector(output_file, sum);
+  // output_file.close();
 
-  Vector vct1 = get_vector();
-  Vector vct2 = get_vector();
-  Vector sum = { "Sum", 0, 0 };
-  calc_sum(vct1, vct2, sum);
+  // 4. Vector Subtraction
+  // ---------------------
+  // Vector vct1 = get_vector();
+  // Vector vct2 = get_vector();
+  // Vector diff = { "Difference", 0, 0 };
+  // calc_difference(vct1, vct2, diff);
+  //
+  // std::ofstream output_file;
+  // output_file.open("output.txt");
+  // print_vector(output_file, vct1);
+  // print_vector(output_file, vct2);
+  // print_vector(output_file, diff);
+  // output_file.close();
 
-  print_vector(output_file, vct1);
-  print_vector(output_file, vct2);
-  print_vector(output_file, sum);
-  
-  output_file.close();
+  // 5. Scalar Multiple
+  // ------------------
+  // Vector vct1 = get_vector();
+  // Vector scalar_multiple = { "Scalar Multiple", 0, 0 };
+  // calc_scalar_multiple(vct1, scalar_multiple, 10);
+  //
+  // std::ofstream output_file;
+  // output_file.open("output.txt");
+  // print_vector(output_file, vct1);
+  // print_vector(output_file, scalar_multiple);
+  // output_file.close();
+
+  // 6. Scalar Product
+  // -----------------
+  // Vector vct1 = get_vector();
+  // Vector vct2 = get_vector();
+  // double scalar_product;
+  // calc_scalar_product(vct1, vct2, scalar_product);
+  //
+  // std::ofstream output_file;
+  // output_file.open("output.txt");
+  // output_file << "Scalar product = " << scalar_product << std::endl;
+  // output_file.close();
+
+  // 7. Vector Magnitude
+  // -------------------
+  // Vector vct1 = get_vector();
+  // double magnitude;
+  // calc_vector_magnitude(vct1, magnitude);
+  //
+  // std::ofstream output_file;
+  // output_file.open("output.txt");
+  // output_file << "Magnitude of " << vct1.name << " = " << magnitude << std::endl;
+  // output_file.close();
+
   return 0;
 }
--- src/week1/vector.hpp
+++ src/week1/vector.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 struct Vector {
   std::string name;
@@ -24,14 +25,36 @@ Vector get_vector() {
   return vct;
 }
 
-void print_vector(std::ofstream& output_file, Vector v) {
-  output_file << "Vector " << v.name << " = (" << v.x << ", " << v.y << ")" << std::endl;
+void print_vector(std::ofstream& output_file, Vector v, std::string line_prefix = "") {
+  if (line_prefix == "") {
+    output_file << v.name << " = (" << v.x << ", " << v.y << ")" << std::endl;
+  } else {
+    output_file << line_prefix << " " << v.name << " = (" << v.x << ", " << v.y << ")" << std::endl;
+  }
 }
 
 void calc_sum(Vector v1, Vector v2, Vector& sum) {
-  double x_sum = v1.x + v2.x;
-  double y_sum = v1.y + v2.y;
-  sum.x = x_sum;
-  sum.y = y_sum;
+  sum.x = v1.x + v2.x;
+  sum.y = v1.y + v2.y;
+}
+
+void calc_difference(Vector v1, Vector v2, Vector& diff) {
+  diff.x = v1.x - v2.x;
+  diff.y = v1.y - v2.y;
+}
+
+void calc_scalar_multiple(Vector v1, Vector& scalar_prod, double multiple) {
+  scalar_prod.x = multiple * v1.x;
+  scalar_prod.y = multiple * v1.y;
+}
+
+void calc_scalar_product(Vector v1, Vector v2, double& scalar_product) {
+  scalar_product = (v1.x * v2.x) + (v1.y * v2.y);
+}
+
+void calc_vector_magnitude(Vector v1, double& magnitude) {
+  double x_squared = pow(v1.x, 2);
+  double y_squared = pow(v1.y, 2);
+  magnitude = pow(x_squared + y_squared, 0.5);
 }
 #endif
